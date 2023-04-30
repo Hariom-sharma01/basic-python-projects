a578d08f0f0ef051ec7df2152c89b26438b089b3
Hariom Sharma <122799261+Hariom-sharma01@users.noreply.github.com>
2023-04-30 10:01:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
-# secure password
 import random
-def password():
-    s=''
-    l1=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    l2=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    l3=['0','1','2','3','4','5','6','7','8','9']
-    l4=['@','#','&','*','$']
-    for  i in range(14):
-        if i<=7:
-            s+=random.choice(l1)
-        elif i>7 and i<=9:
-            s+=random.choice(l2)
-        elif i>9 and i<=12:
-            s+=random.choice(l3)
-        else:
-            s+=random.choice(l4)
-    print('Your secure password is::',s)
-print('To generate your password press enter')
-input()
-password()
+def game():
+    l1=['Rock','Paper','Scissor']
+    a=random.choice(l1) 
+    print('Please select one option against computer from the given!!')
+    print('Rock::\n','Paper::\n','Scissor::')
+    t1=input()
+    t=t1.title()
+    if t=='Rock' and a=='Scissor':
+        print('You win!!')
+    elif t=='Rock' and a=='Paper':
+        print('You loss')
+    elif t=='Rock' and a=='Rock':
+        print("it's draw ")
+    elif t=='Paper' and a=='Scissor':
+        print('You loss ')
+    elif t=='Paper' and a=='Paper':
+        print("it's draw ")
+    elif t=='Paper' and a=='Rock':
+        print('You win!!')
+    elif t=='Scissor' and a=='Scissor':
+        print("it's draw ")
+    elif t=='Scissor' and a=='Paper':
+        print('You win!!')
+    else:
+        print('You loss')
+    print('Your move::',t,'Vs')
+    print("computer's move::",a)
+    print('Do you want to play again??y/n')
+    s=input()
+    return s
+print('Welcome to the game'.center(100))
+print('Rock::Vs::Paper::Vs::Scissor')
+print('Do you want to try it??:y/n')
+s1=input()
+if s1 in 'Yy':
+    re=game()
+    while(re not in 'Nn'):
+        re=game()
+    print('Thank You!')
+else:
+    print('Thank You!')
+        
+   
+
